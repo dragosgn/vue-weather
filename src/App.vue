2d426f4e00dc8d1f7487ceb3497edbdb24125e03
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-02 18:00:51 +0200

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>hello</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      api_key: "53378ee977b0303b6607dd81e9c50a3b"
+    }
   }
+ 
 }
 </script>
 
