874b5bcde216c02a98ca15de86c3d963f0e0492b
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-02 19:12:49 +0200

    fixed api

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." />
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northhamptopn, UK</div>
-          <div class="date">Monday 20 Jan 2020</div>
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
+          <div class="date">Monday 20 January 2020</div>
         </div>
         <div class="weather-box">
           <div class="temp">9°c</div>
@@ -23,8 +29,28 @@ export default {
   name: "App",
   data() {
     return {
-      api_key: "53378ee977b0303b6607dd81e9c50a3b"
+      api_key: "53378ee977b0303b6607dd81e9c50a3b",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {}
     };
+  },
+  methods: {
+    fetchWeather(e) {
+      console.log(e);
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then(resp => {
+            return resp.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    }
   }
 };
 </script>
