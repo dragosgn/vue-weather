7871f2a4d0531d4e472a3e964cee3ee6db42a8a6
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-03 11:51:38 +0200

    adte builder

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
@@ -50,6 +50,39 @@ export default {
     },
     setResults(results) {
       this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 };
